9d74a4dfb2f57aa0d8e93194bca41d0e38ee8d4d
Doc082 <mirkofenu@yahoo.it>
2023-10-22 17:06:27 +0200

    Prima versione on-line

--- footer.php
+++ footer.php
@@ -2,9 +2,14 @@
  <div class="diagotwo"></div>
     <div class="container-fluid">
       <div id="footer" class="position-relative overflow-hidden p-3 p-md-5 m-md-2">
-        <div class="offset-lg-3 col-md-6 p-lg-5 my-5 text-center">
-            <p>Sito di Majla Fadda</p>
-            <p>Tutti i diritti riservati</p>
+        <div class="offset-3 col-md-6 p-lg-5 my-5 text-center">
+            <h3>Majla Fadda Scrittrice</h3>
+            <h4>Vieni a trovarmi sulle mie pagine social</h4>
+            <i class="fa-solid fa-circle-arrow-down fa-beat-fade fa-2xl mt-5" style="color: #0000ee;"></i>
+            <div class="d-flex justify-content-center fw-normal mt-3">
+                <a href="https://www.instagram.com/majla_fadda" class="me-2"><img class="social" src="<?php echo get_theme_file_uri('images/insta.png');?>" alt="Condividi su Instagram"/></a>
+                <a href="https://www.facebook.com/profile.php?id=61550895517748"><img class="social" src="<?php echo get_theme_file_uri('images/facebook.png');?>" alt="Condividi su Facebook"/></a>
+            </div>
           </div>
         </div>
       </div>
--- front-page.php
+++ front-page.php
@@ -5,13 +5,13 @@
         <h1 class="display-6 fw-bold">Esplora mondi nascosti tra le pagine. Benvenuto nel rifugio delle parole, dove l'immaginazione prende vita.</h1>
         <h3 class="fw-normal text-muted mb-3"></h3>
         <div class="d-flex justify-content-md-start justify-content-center fw-normal">
-          <a class="btn btn-primary" href="#">
+          <a class="btn btn-primary" href="/la-compagnia-di-giustino">
             Scopri il potere delle parole!
           </a>
         </div>
         <div class="d-flex justify-content-md-start justify-content-center fw-normal mt-5">
-          <a href="#" class="me-2"><img class="social" src="<?php echo get_theme_file_uri('images/insta.png');?>" alt="Condividi su Instagram"/></a>
-          <a href="#"><img class="social" src="<?php echo get_theme_file_uri('images/facebook.png');?>" alt="Condividi su Facebook"/></a>
+          <a href="https://www.instagram.com/majla_fadda" class="me-2"><img class="social" src="<?php echo get_theme_file_uri('images/insta.png');?>" alt="Condividi su Instagram"/></a>
+          <a href="https://www.facebook.com/profile.php?id=61550895517748"><img class="social" src="<?php echo get_theme_file_uri('images/facebook.png');?>" alt="Condividi su Facebook"/></a>
         </div>
       </div>
       <div class="img_right shadow-sm d-none d-md-block shadow-sm"></div>
@@ -28,17 +28,17 @@
             $count = 0;
             while($evidenceBook->have_posts()){
                 $evidenceBook->the_post();
-                if($count) echo "<hr class='hr'/>";
+                if($count) echo "<hr class='border border-primary border-2 opacity-25'/>";
         ?>
         <div class="row p-3 m-3">
             <?php if($count == 1) {?>
-            <div class="offset-md-4 col-md-4 col-sm-12 order-md-2 text-md-start text-center"><a href="<?php the_permalink(); ?>"><img class="img_thumb shadow" src="<?php echo the_post_thumbnail_url()?>" alt="<?php echo the_post_thumbnail_caption()?>" /></a></div>
-            <div class="col-md-4 col-sm-12 text-md-start text-center">
+            <div class="offset-lg-1 col-lg-4 col-md-12 order-lg-2 text-lg-start text-center"><a href="<?php the_permalink(); ?>"><img class="img_thumb shadow" src="<?php echo the_post_thumbnail_url()?>" alt="<?php echo the_post_thumbnail_caption()?>" /></a></div>
+            <div class="offset-lg-2 col-lg-4 col-md-12 text-lg-start text-center">
               <?php }else{?>
-                <div class="col-md-4 col-sm-12 text-md-start text-center"><a href="<?php the_permalink(); ?>"><img class="img_thumb shadow" src="<?php echo the_post_thumbnail_url()?>" alt="<?php echo the_post_thumbnail_caption()?>" /></a></div>
-            <div class="offset-md-4 col-md-4 col-sm-12 text-md-start text-center">
+                <div class="offset-lg-2 col-lg-4 col-md-12 text-lg-start text-center"><a href="<?php the_permalink(); ?>"><img class="img_thumb shadow" src="<?php echo the_post_thumbnail_url()?>" alt="<?php echo the_post_thumbnail_caption()?>" /></a></div>
+            <div class="offset-lg-1 col-lg-4 col-md-12 text-lg-start text-center">
               <?php }?>
-                <p><?php echo wp_trim_words(get_the_content(), 20); ?><br><span><a href="<?php the_permalink(); ?>"><i class="fa-regular fa-square-caret-right fa-beat" style="color: #0000EE;"></i> <b>Leggi tutto...</b></a></span></p>
+                <p class="h3"><?php echo wp_trim_words(get_the_content(), 20); ?><br><span><a href="<?php the_permalink(); ?>"><i class="fa-regular fa-square-caret-right fa-beat" style="color: #0000EE;"></i> <b>Leggi tutto...</b></a></span></p>
             </div>
         </div>
         <?php
@@ -56,7 +56,7 @@
           <h3 class="fw-normal fw-bold">Il viaggio inizia qui</h3>
           <h3 class="fw-normal text-muted mb-3">Ma l'avventura continua nel mio mondo. Scegli di rimanere e scoprire ancora di piu'.</h3>
           <div class="d-flex justify-content-center fw-normal">
-            <a class="btn btn-primary" href="#">
+            <a class="btn btn-primary" href="/la-compagnia-di-giustino">
               Prenota la tua avventura letteraria!
             </a>
           </div>
@@ -77,12 +77,12 @@
           while($newBooks->have_posts()){
               $newBooks->the_post();
               ?>
-        <div class="col-md-4 col-sm-12 d-flex justify-content-center">
-          <div class="card card_height" style="width: 18rem; margin-bottom: 15px;">
+        <div class="col-lg-4 col-md-12 d-flex justify-content-center">
+          <div class="card card_height" style="width: 22rem; margin-bottom: 15px;">
             <img class="card-img-top" src="<?php the_post_thumbnail_url()?>" alt="<?php the_post_thumbnail_caption(); ?>">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column">
               <p class="card-text"><?php the_excerpt();?></p>
-              <a href="#" class="btn btn-primary">Aquista Libro</a>
+              <a href="<?php the_permalink();?>" class="mt-auto btn btn-primary">Leggi di più</a>
             </div>
           </div>
         </div>
@@ -90,9 +90,6 @@
       </div>
     </div>
 
-  <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>-->
     <?php get_footer();?>
     <?php wp_footer();?>
 </body>
